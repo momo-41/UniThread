721f9c8ecb03d48a6f11d758a50be2631a4a8056
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-15 16:27:07 +0900

    Articleコンポーネント設置

--- app/article/1/page.tsx
+++ app/article/1/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+// app/article/1/page.tsx
+"use client";
+import Article from "../../component/Article";
 
-const ArticleDetailpage = () => {
-  return <div>ArticleDetailpage</div>;
-};
-
-export default ArticleDetailpage;
+export default function ArticleDetailPage() {
+  return (
+    <Article
+      date="2025/8/11"
+      title="細胞生化学を落とさないために"
+      text={`2年前期で受講して3単位...（導入文）`}
+    />
+  );
+}
