d80a39a4e432c7f302c3487edee26f8a086d7fe8
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-15 14:12:29 +0900

    ぽんおき

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ArticleCard from "../component/ArticleCard";
 
 const ArticlePage = () => {
-  return <div>Article Page</div>;
+  return (
+    <div>
+      <ArticleCard
+        title={"【1年生】初めての期末テストで絶対に確認しておくこと"}
+        authorName={"みんみん"}
+      />
+    </div>
+  );
 };
 
 export default ArticlePage;
