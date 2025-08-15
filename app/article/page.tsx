dff37de34087d045794eaef326efe674c9572729
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-15 14:19:03 +0900

    Postボタンぽんおき

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ArticleCard from "../component/ArticleCard";
+import PostButton from "../component/PostButton";
 
 const ArticlePage = () => {
   return (
     <div>
+      <PostButton />
       <ArticleCard
         title={"【1年生】初めての期末テストで絶対に確認しておくこと"}
         authorName={"みんみん"}
