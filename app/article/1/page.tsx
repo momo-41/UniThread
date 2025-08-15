9cf8929dce4fe841c85895cdffc2608a329ff4e2
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-15 20:42:04 +0900

    Articleコンポーネント関数名変更

--- app/article/1/page.tsx
+++ app/article/1/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import Article from "../../component/Article";
+import ArticleDetail from "../../component/ArticleDetail";
 
 export default function ArticleDetailPage() {
   return (
-    <Article
+    <ArticleDetail
       title="[1年生] 初めての期末テストで絶対に確認しておくこと"
       content={`記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く\n 記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く\n 記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く`}
     />
