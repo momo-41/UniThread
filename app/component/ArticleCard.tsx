1ce913c4e2c5fd820f08a61406b91fcf229b42eb
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-15 15:21:09 +0900

    props名変更

--- app/component/ArticleCard.tsx
+++ app/component/ArticleCard.tsx
@@ -7,30 +7,32 @@ import {
   CardActionArea,
   CardContent,
   Typography,
-  Avatar,
   Stack,
 } from "@mui/material";
 
-type Props = {
+export type ArticleCardProps = {
   authorName: string;
   title: string;
   onClick?: () => void;
 };
 
-const ArticleCard: React.FC<Props> = ({ authorName, title, onClick }) => {
+const ArticleCard: React.FC<ArticleCardProps> = ({
+  authorName,
+  title,
+  onClick,
+}) => {
   return (
     <Card variant="outlined" sx={{ borderRadius: 2 }}>
       <CardActionArea onClick={onClick}>
         <CardContent>
-          <Stack direction="row" spacing={1.5} alignItems="center" mb={1}>
-            <Avatar />
+          <Stack spacing={0.5}>
             <Typography variant="body2" color="text.secondary">
               {authorName}
             </Typography>
+            <Typography variant="subtitle1" fontWeight={700}>
+              {title}
+            </Typography>
           </Stack>
-          <Typography variant="subtitle1" fontWeight={700}>
-            {title}
-          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
