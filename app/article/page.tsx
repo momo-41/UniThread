169f80c299f25dae26df4ea011c93c302c77119e
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-18 05:18:50 +0900

    fechしてページに表示。データがなければ記事が投稿されていませんを表示

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import PostButton from "../component/PostButton";
 import ArticleCardList from "../component/ArticleCardList";
+import { headers } from "next/headers";
 
 const ArticlePage = async () => {
-  const response = await fetch("http://localhost:3000/api/article", {
-    cache: "no-store",
-  });
-  const AllArticlePosts = await response.json();
-  console.log(AllArticlePosts);
+  async function getAllArticleposts() {
+    const response = await fetch(`${process.env.APP_URL!}/api/article`, {
+      cache: "no-store",
+      headers: { cookie: (await headers()).get("cookie") ?? "" },
+    });
+    const allArticlePosts = await response.json();
+    return allArticlePosts;
+  }
+
+  const allArticlePosts = await getAllArticleposts();
+
   return (
     <div>
       <PostButton />
-      <ArticleCardList
-        authorName="みんみん"
-        title="[1年生] 初めての期末テストで絶対に確認しておくこと"
-        articleId={"1"}
-      />
+      {allArticlePosts.length === 0 ? (
+        <div style={{ padding: 8, color: "#666" }}>
+          記事が投稿されていません
+        </div>
+      ) : (
+        allArticlePosts.map(({ id, title, author }: any) => (
+          <ArticleCardList
+            key={id}
+            authorName={author.displayName}
+            title={title}
+            articleId={id}
+          />
+        ))
+      )}
     </div>
   );
 };
