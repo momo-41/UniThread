3c3200e2c1ee6274e31ff2220f935e2abc4c40e1
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-18 03:28:20 +0900

    コメントアウト修正

--- app/api/article/route.ts
+++ app/api/article/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const AllArticlePosts = await prisma.article.findMany();
-  // データが増えてきたら10件だけまず取得して、次へを押すともう10件取得のようにする方がいいらしい。findmany()は仮に全件取得している。
+  // データが増えてきたら10件だけまず取得して、次へを押すともう10件取得のようにする方がいいらしい。
+  // findmany()は仮に全件取得している。
   return NextResponse.json(AllArticlePosts);
 }
