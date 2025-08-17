4aa737de06dbb163739c083d55a4d54f4dab162c
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-18 03:25:02 +0900

    prismaのベストプラクティス修正

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,12 +1,9 @@
-import "server-only";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma;
+//prismaのインスタンスの発火を制御できる
