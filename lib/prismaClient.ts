091cb842ddd9da3297b94250834fed7942d90540
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-17 11:29:57 +0900

    prismaのベストプラクティス実行

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,1 +1,14 @@
-// prismaClient.tsの追加
+import { PrismaClient } from "@prisma/client";
+import "server-only";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = globalThis.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
