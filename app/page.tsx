a0f314038bba1d537c2104067e29e47d6bd2ffda
Shin <xxin11873@gmail.com>
2025-08-20 18:47:33 +0900

    講義カードの作成の修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+const threadPage = () => {
   return (
-    <>
-      <div>aaa</div>
-    </>
+    <Card
+      sx={{
+        width: 300,
+        height: 90,
+        transition: "0.3s",
+        "&:hover": {
+          background: "#DCF5F7",
+        },
+      }}
+    >
+      <CardContent>
+        <Typography color={"#383838"} fontSize={18} fontWeight={500}>
+          講義名講義名
+        </Typography>
+      </CardContent>
+    </Card>
   );
-}
+};
+
+export default threadPage;
