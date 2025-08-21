afec7528446250f5c712654276c04f7ef6dcbb49
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-22 04:11:58 +0900

    postページに検索に戻るボタン追加

--- app/article/post/page.tsx
+++ app/article/post/page.tsx
@@ -1,9 +1,15 @@
 import ArticlePostForm from "@/app/component/ArticlPostForm";
+import { Button } from "@mui/material";
 import React from "react";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
 
 const PostPage = () => {
   return (
     <div>
+      <Button href="/article">
+        <ArrowBackIosNewRoundedIcon fontSize="small" />
+        検索に戻る
+      </Button>
       <ArticlePostForm />
     </div>
   );
