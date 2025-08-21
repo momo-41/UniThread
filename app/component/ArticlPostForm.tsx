d270f8cf0b40636e311af1afcc7d68384095523e
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-22 03:10:17 +0900

    記事入力コンポーネントを作成

---
+++ app/component/ArticlPostForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const FormSchema = z.object({
+  authorId: z.string().uuid(),
+  title: z.string(),
+  content: z.string(),
+});
+
+type FormValues = z.infer<typeof FormSchema>;
+
+export default function ArticlePostForm() {
+  const { register, handleSubmit } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = async (data: FormValues) => {
+    await fetch(`${process.env.APP_URL!}/api/article`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>authorId</label>
+        <input type="text" {...register("authorId")} />
+      </div>
+      <div>
+        <label>title</label>
+        <input type="text" {...register("title")} />
+      </div>
+      <div>
+        <label>content</label>
+        <textarea {...register("content")} />
+      </div>
+      <button type="submit">投稿</button>
+    </form>
+  );
+}
