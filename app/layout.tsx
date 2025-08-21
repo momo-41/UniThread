24f310e34e3e4b1c303ab0504840118cec2895bc
momo-41 <minimizu41@gmail.com>
2025-08-21 18:17:58 +0900

    clerkの認証画面を日本語表示にするためのコードを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./component/Header";
+import { jaJP } from "@clerk/localizations";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={jaJP}>
       <html lang="ja">
         <body style={{ margin: 0 }}>
           <Header />
