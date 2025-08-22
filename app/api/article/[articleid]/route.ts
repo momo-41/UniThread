8dbc91134abcfcffab2f30fb240c4328b84f5bc4
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-23 06:58:24 +0900

    記事取得api修正（データがないことによるクラッシュ予防・返り値にcontent追加）

--- app/api/article/[articleid]/route.ts
+++ app/api/article/[articleid]/route.ts
@@ -3,15 +3,20 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   _req: Request,
-  { params }: { params: { bbsid: string } }
+  { params }: { params: { articleid: string } }
 ) {
   const ArticleDetailData = await prisma.article.findUnique({
-    where: { id: params.bbsid },
+    where: { id: params.articleid },
     select: {
       id: true,
       title: true,
+      content: true,
       author: { select: { handle: true, displayName: true } },
     },
   });
+
+  if (!ArticleDetailData) {
+    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+  }
   return NextResponse.json(ArticleDetailData);
 }
