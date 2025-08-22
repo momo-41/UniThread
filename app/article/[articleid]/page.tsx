8878a78cee57269cc2c6596201ebaadf72f4365a
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-23 06:53:01 +0900

    記事詳細ページにapiを適応・typescriptの適応

--- app/article/[articleid]/page.tsx
+++ app/article/[articleid]/page.tsx
@@ -1,18 +1,25 @@
 import { headers } from "next/headers";
 import ArticleDetail from "../../component/ArticleDetail";
+
 async function getArticleDetailData(id: string) {
   const response = await fetch(`${process.env.APP_URL!}/api/article/${id}`, {
     cache: "no-store",
     headers: { cookie: (await headers()).get("cookie") ?? "" },
   });
-  const ArticleDetailData = await response.json();
-  return ArticleDetailData;
+  const articleDetailData = await response.json();
+  return articleDetailData;
 }
-export default function ArticleDetailPage() {
+
+type Props = { params: { articleid: string } };
+
+export default async function ArticleDetailPage(props: Props) {
+  const { articleid } = props.params;
+  const articleDetail = await getArticleDetailData(articleid);
+
   return (
     <ArticleDetail
-      title="[1年生] 初めての期末テストで絶対に確認しておくこと"
-      content={`記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く\n 記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く\n 記事投稿の本文を書く記事投稿の本文を書く記事投稿の本文を書く`}
+      title={articleDetail.title}
+      content={articleDetail.content}
     />
   );
 }
