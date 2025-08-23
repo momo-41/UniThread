6c4ee26cd8b6fdd4b896b6fece9fc3ed28731c95
MIYAKOES <miyakokkoa385@gmail.com>
2025-08-23 17:36:02 +0900

    要素の配置の修正と色を変更とフォントサイズの変更

--- app/component/ThreadCard.tsx
+++ app/component/ThreadCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Box, CardContent, Typography, Avatar } from "@mui/material";
+import { Card, Box, Typography, Avatar } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 
 const ThreadCard = () => {
@@ -6,35 +6,29 @@ const ThreadCard = () => {
     <Card
       sx={{
         width: 300,
-        height: "100%",
         borderRadius: 3,
         display: "flex",
       }}
     >
-      <Box
-        width={8}
-        height="100%"
-        bgcolor="#D1E6E8"
-        borderRadius="8px 0 0 8px"
-      />
-      <CardContent>
-        <Typography fontSize={20} fontWeight="bold">
+      <Box width={8} bgcolor={"#D9D9D9"} borderRadius={"8px 0 0 8px"} />
+      <Box width={"100%"} p={1}>
+        <Typography fontSize={17} fontWeight={"bold"} color={"#444444"}>
           中間の範囲について
         </Typography>
-        <Box display="flex" justifyContent="space-between">
-          <Box display="flex" alignItems="center">
+        <Box display={"flex"} justifyContent={"space-between"} mt={2}>
+          <Box display={"flex"} alignItems={"center"}>
             <Avatar sx={{ width: 24, height: 24 }}>
               <PersonIcon sx={{ fontSize: 20 }} />
             </Avatar>
-            <Typography fontSize={14} ml={1} color="text.secondary">
+            <Typography fontSize={14} ml={1} color={"#444444"}>
               momo
             </Typography>
           </Box>
-          <Typography fontSize={12} color="text.secondary">
+          <Typography fontSize={12} color={"#444444"} mt={1.5}>
             2025/8/11
           </Typography>
         </Box>
-      </CardContent>
+      </Box>
     </Card>
   );
 };
