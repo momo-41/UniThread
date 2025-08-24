434f511a94f0ced2a92f716a6b7aa9e4d9eaa80d
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-24 17:28:16 +0900

    ディレクトリのidをIdに変更

---
+++ app/api/article/[articleId]/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/lib/prismaClient";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  params: { params: Promise<{ articleId: string }> }
+) {
+  const { articleId } = await params.params;
+
+  const data = await prisma.article.findUnique({
+    where: { id: articleId },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      author: { select: { handle: true, displayName: true } },
+    },
+  });
+
+  if (!data) {
+    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+  }
+  return NextResponse.json(data);
+}
