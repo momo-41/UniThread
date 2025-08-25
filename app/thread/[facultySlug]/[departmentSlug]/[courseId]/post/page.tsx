1e71df3a3f8c4732f8456df3f42bc90dd7b94b18
YoshiakiAsahara <asa-yossan@outlook.jp>
2025-08-25 20:24:26 +0900

    promise await追加してnext15に対応

--- app/thread/[facultySlug]/[departmentSlug]/[courseId]/post/page.tsx
+++ app/thread/[facultySlug]/[departmentSlug]/[courseId]/post/page.tsx
@@ -1,20 +1,22 @@
 import { notFound } from "next/navigation";
-import { prisma } from "@/lib/prismaClient";
+import { prisma } from "@/lib/prismaClient"; // named exportなら { prisma } に
 import ThreadPostForm from "@/app/component/ThreadPostForm";
 
 export default async function Page({
   params,
 }: {
-  params: { facultySlug: string; departmentSlug: string; courseId: string };
+  params: Promise<{
+    facultySlug: string;
+    departmentSlug: string;
+    courseId: string;
+  }>;
 }) {
-  const { facultySlug, departmentSlug, courseId } = params;
-
+  const { facultySlug, departmentSlug, courseId } = await params;
   const course = await prisma.course.findUnique({
     where: { id: courseId },
     select: { id: true, courseName: true },
   });
   if (!course) notFound();
-
   return (
     <ThreadPostForm
       facultySlug={facultySlug}
