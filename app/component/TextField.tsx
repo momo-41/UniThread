72efeaa23fb45cf5e295c1c9b68eb81c4bd82990
Shin <xxin11873@gmail.com>
2025-08-26 22:25:25 +0900

    学部のページに検索バーを表示(入力のみ)

---
+++ app/component/TextField.tsx
@@ -0,0 +1,29 @@
+import { Box, TextField as MuiTextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+
+const CustomTextField = () => {
+  return (
+    <Box
+      component="form"
+      sx={{ "& > :not(style)": { mt: 2, width: "50ch" } }}
+      autoComplete="off"
+      display={"flex"}
+      justifyContent={"center"}
+    >
+      <MuiTextField
+        id="outlined-basic"
+        placeholder="講義名・投稿内容・タグで検索"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
+  );
+};
+
+export default CustomTextField;
--- app/thread/page.tsx
+++ app/thread/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FacultyCardList from "../component/FacultyCardList";
+import TextField from "../component/TextField";
 
 const threadPage = () => {
   return (
     <div>
+      <TextField />
       <FacultyCardList />
     </div>
   );
