86c40bca5390374d0e776bc3a689e64ceaab9de8
MIYAKOES <miyakokkoa385@gmail.com>
2025-08-27 01:26:19 +0900

    CourseSearchButton.tsxを作成。Propsを用いて作った学年ボタンを講義一覧ページに配置(2回目)

---
+++ app/component/CourseSearchButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@mui/material";
+type CourseSearchButtonProps = {
+  text: string;
+};
+
+const CourseSearchButton: React.FC<CourseSearchButtonProps> = ({ text }) => {
+  return (
+    <Button variant="outlined" sx={{ color: "#32A7B4" }}>
+      {text}
+    </Button>
+  );
+};
+
+const CourseSearchButtonGroup: React.FC = () => {
+  return (
+    <div>
+      <CourseSearchButton text="全学年" />
+      <CourseSearchButton text="1年生" />
+      <CourseSearchButton text="2年生" />
+      <CourseSearchButton text="3年生" />
+      <CourseSearchButton text="4年生" />
+    </div>
+  );
+};
+
+export default CourseSearchButtonGroup;
