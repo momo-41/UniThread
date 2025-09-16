1038e92f7ecb5ca95a664a29ae6dfd440e32d1d9
MIYAKOES <miyakokkoa385@gmail.com>
2025-09-16 10:40:21 +0900

    検索ボックスのレスポンシブ対応

--- app/component/SearchBox.tsx
+++ app/component/SearchBox.tsx
@@ -5,7 +5,12 @@ const SearchBox = () => {
   return (
     <Box
       component="form"
-      sx={{ "& > :not(style)": { mt: 2, width: "50ch" } }}
+      sx={{
+        "& > :not(style)": {
+          mt: 2,
+          width: { xs: "40ch", md: "50ch" },
+        },
+      }}
       autoComplete="off"
       display={"flex"}
       justifyContent={"center"}
