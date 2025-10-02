adab7da264709ef6ab819b741233b62f14501100
Shin <xxin11873@gmail.com>
2025-10-02 23:29:19 +0900

    学部カードリストのレスポンシブ対応

--- app/component/FacultyCardList.tsx
+++ app/component/FacultyCardList.tsx
@@ -6,7 +6,13 @@ import { FacultyDepartmentData } from "@/data/faculty-department-data";
 const FacultyCardList = () => {
   return (
     <>
-      <Typography fontWeight={550} fontSize={20} color="#383838" ml={3} mt={1}>
+      <Typography
+        fontWeight={550}
+        fontSize={{ xs: 18, md: 20 }}
+        color="#383838"
+        ml={{ xs: 2, md: 3 }}
+        mt={1}
+      >
         受けている講義の学部
       </Typography>
       <Grid container spacing={2} m={2}>
